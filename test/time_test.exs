09d5262f6d28d708d2c411c01c276bfb69484d5a
Fabian Keunecke <fkeunecke@frobese.de>
2015-07-29 17:59:25 +0200

    Add usec: 0 as default to time test

--- test/time_test.exs
+++ test/time_test.exs
@@ -2,14 +2,14 @@
 defmodule TimeTest do
   use ExUnit.Case
 
-  @time %Calendar.Time{hour: 10, min: 42, sec: 53}
+  @time %Calendar.Time{hour: 10, min: 42, sec: 53, usec: 0}
   @time_with_usec %Calendar.Time{hour: 10, min: 42, sec: 53, usec: 12}
-  @time_without_sec %Calendar.Time{hour: 10, min: 42, sec: 0}
+  @time_without_sec %Calendar.Time{hour: 10, min: 42, sec: 0, usec: 0}
   # @map_time %{"hour" => "10", "min" => "42", "sec" => "53"}
   # @map_time_without_sec %{"hour" => "10", "min" => "42"}
-  @tuple_time {10, 42, 53}
+  @tuple_time {10, 42, 53, 0}
   @tuple_time_with_usec {10, 42, 53, 12}
-  @tuple_time_without_sec {10, 42, 0}
+  @tuple_time_without_sec {10, 42, 0, 0}
 
   test "dump Time" do
     assert Calecto.Time.dump(@time) == {:ok, @tuple_time}
