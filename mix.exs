b92b3de8d29b94797e4351a64c2ad82891330dc1
Lau Taarnskov <lau@taarnskov.com>
2015-12-10 21:58:13 -0300

    update exdocs

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Calecto.Mixfile do
       {:calendar, "~> 0.12.0 or ~> 0.11.1 or ~> 0.10.1"},
 
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.8", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
     ]
   end
 
