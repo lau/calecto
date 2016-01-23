0844abfca8317bd728996f417e6c8540e8c417e9
Lau Taarnskov <lau@taarnskov.com>
2016-01-23 17:52:25 -0300

    Deprecate Ecto.Model in favour of Ecto.Schema

--- lib/calecto/model.ex
+++ lib/calecto/model.ex
@@ -1,49 +1,9 @@
 defmodule Calecto.Model do
   @moduledoc """
-  Provides a `use` macro for adding Calendar support to Ecto timestamps.
+  This module name is deprecated in favor of `Calecto.Schema`
+  Simply use the model name `Calecto.Schema` instead of `Calecto.Model`.
 
-  It also takes an argument to define whether microseconds should be
-  used for Ecto timestamps. It is recommended to set this to true
-  unless a MySQL database older than 5.0 is used.
-
-  The macro can either be added in the Ecto models where schema definitions
-  exist.
-
-  Example:
-
-  ```
-  defmodule Weather do
-    use Ecto.Model
-    use Calecto.Model, usec: true
-
-    schema "weather" do
-      field :city, :string
-      timestamps
-    end
-  end
-  ```
-
-  ## Usage with Phoenix
-
-  In Phoenix the file `web/web.ex` contains a macro that is by default used in
-  all models. This means that you can simply add the `use Calecto.Model` line
-  in that file once, and it will be used in all models.
-
-  Find the method defintion `model` and add the `use Calecto.Model` line under
-  `use Ecto.Model`:
-
-  ```elixir
-  # web/web.ex
-  # ...
-
-  def model do
-    quote do
-      use Ecto.Model
-      use Calecto.Model, usec: true
-    end
-  end
-  ```
-
+  See docs for `Calecto.Schema`
   """
   @default_timestamps_opts [type: Calecto.DateTimeUTC]
   defmacro __using__(opts) do
---
+++ lib/calecto/schema.ex
@@ -0,0 +1,54 @@
+defmodule Calecto.Schema do
+  @moduledoc """
+  Provides a `use` macro for adding Calendar support to Ecto timestamps.
+
+  It also takes an argument to define whether microseconds should be
+  used for Ecto timestamps. It is recommended to set this to true
+  unless a MySQL database older than 5.0 is used.
+
+  The macro can either be added in the Ecto models where schema definitions
+  exist.
+
+  Example:
+
+  ```
+  defmodule Weather do
+    use Ecto.Schema
+    use Calecto.Schema, usec: true
+
+    schema "weather" do
+      field :city, :string
+      timestamps
+    end
+  end
+  ```
+
+  ## Usage with Phoenix
+
+  In Phoenix the file `web/web.ex` contains a macro that is by default used in
+  all models. This means that you can simply add the `use Calecto.Schema` line
+  in that file once, and it will be used in all models.
+
+  Find the method defintion `model` and add the `use Calecto.Schema` line under
+  `use Ecto.Schema`:
+
+  ```elixir
+  # web/web.ex
+  # ...
+
+  def model do
+    quote do
+      use Ecto.Schema
+      use Calecto.Schema, usec: true
+    end
+  end
+  ```
+
+  """
+  @default_timestamps_opts [type: Calecto.DateTimeUTC]
+  defmacro __using__(opts) do
+    quote do
+      @timestamps_opts unquote(Dict.merge(opts, @default_timestamps_opts))
+    end
+  end
+end
