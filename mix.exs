ec05ffdca02134ec0232210903c31ff73bacd9ac
Eugene Maslenkov <maslenkov@i.ua>
2016-10-07 13:29:45 +0300

    update ecto version

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Calecto.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 1.1.3 or ~> 2.0.0 or ~> 2.0.0-rc5"},
+      {:ecto, "~> 1.1.3 or ~> 2.0.0 or ~> 2.0.0-rc5 or ~> 2.1.0-rc.1"},
       {:calendar, "~> 0.16.0"},
 
       {:earmark, "~> 0.2.1", only: :dev},
